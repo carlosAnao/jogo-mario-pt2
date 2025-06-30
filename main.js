bb225bb61ac7ec3b65dc3cb3b112bca10446ee94
carlosAnao <joao.zibarth.santos@escola.pr.gov.br>
2025-06-30 07:37:36 -0300

    g

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const pipe = document.querySelector(".pipe")
 const nuvem = document.querySelector(".nuvem")
 const startButton = document.querySelector(".start")
 const gameOverScreen = document.querySelector(".game-over")
-
+const scoreElement = document.querySelector(".score")
 
 audioStart = new Audio("./sound/audio_theme.mp3")
 const gameOverSound = new Audio("./sound/audio_gameover.mp3")
 
 let gameStarted = false;
+let score = 0;
 
 const startGame = () => {
     gameStarted = true;
@@ -32,6 +33,13 @@ const jump = () => {
     ,500);
 }
 
+const updateScore = () => {
+    score +=1;
+    scoreElement.textContent = score;
+    const animationSpeed = 
+    pipe.style.animation = `pipe-animation ${animationSpeed}s infinite linear`;
+}
+
 const loop = setInterval(() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
@@ -48,7 +56,6 @@ const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
         mario.style.marginLeft = "50px";
 
         audioStart.pause();
-
         gameOverSound.play();
 
         clearInterval(loop);
